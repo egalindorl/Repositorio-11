6422115de736dc39773edc42535358461b9d1cf9
egalindorl <egalindorl@gmail.com>
2022-04-04 18:34:44 -0500

    added :9

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={britney} className="App-logo" alt="logo" />
         <p>
-          Hello world! It's Britney bitch
+          Hello world! It's Britney bitch :)
         </p>
         
       </header>
