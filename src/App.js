33629ca4739ad5af19d494674da0327b94ca7d6f
egalindorl <egalindorl@gmail.com>
2022-04-04 18:31:23 -0500

    hello world

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import britney from './britney.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={britney} className="App-logo" alt="logo" />
+        <p>
+          Hello world! It's Britney bitch
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+export default App;
